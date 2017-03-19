9555dcbe7d9f2188840ca1ef07ab5aec01369d57
Viktor Hadzhipopov <viktor.hadzhipopov@gmai.com>
2017-03-19 19:39:36 +0200

    add ivy support

---
+++ src/com/google/gwt/sample/stockwatcher/server/CurrencyResource.java
@@ -0,0 +1,10 @@
+package com.google.gwt.sample.stockwatcher.server;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by vhadzhipopov on 18.03.17.
+ */
+@Component
+public class CurrencyResource {
+}
